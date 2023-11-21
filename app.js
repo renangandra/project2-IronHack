42ffcb9d293960c0ab6b67f2aefa30c84a152cad
Celia Angles <celiaangles22@gmail.com>
2023-11-21 12:49:49 +0100

    add new change

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 // require('./DB');
-const express = require('express');// chamando ele pra funcionanr aqui dentro
-const routesAuth = require('./routes/auth');// colocar as rotas em 2 lugar
+const express = require("express"); // chamando ele pra funcionanr aqui dentro
+const routesAuth = require("./routes/auth"); // colocar as rotas em 2 lugar
 
-const hbs = require('hbs');
+const hbs = require("hbs");
 // const connectDB = require('./DB');
-const path = require('path');
-
+const path = require("path");
 
 const app = express(); //pra adicionar valores dentro do express e pois eu posso reutiliza-lo
 
-
-app.use(express.json()) //habilitar o aplicativo para dar a resposta em formato tipo json
+app.use(express.json()); //habilitar o aplicativo para dar a resposta em formato tipo json
 
 app.use(express.urlencoded({ extended: false }));
 // Normalizes the path to the views folder
-app.set("views", path.join(__dirname, ".", "views"));// ele vai procurar na raiz do projeto
+app.set("views", path.join(__dirname, ".", "views")); // ele vai procurar na raiz do projeto
 // Sets the view engine to handlebars
-app.set("view engine", "hbs");// toda a base do view vai ser pelo hbs --> instalar o hbs
+app.set("view engine", "hbs"); // toda a base do view vai ser pelo hbs --> instalar o hbs
 // Handles access to the public folder
-app.use(express.static(path.join(__dirname, ".", "public")));//ele vai procurar os arquivos estaticos: css, html, imagens
+app.use(express.static(path.join(__dirname, ".", "public"))); //ele vai procurar os arquivos estaticos: css, html, imagens
 
+//any change
 
+app.use("/", routesAuth); // funcao pra configurar o que o express vai utilizar
 
-app.use('/', routesAuth)// funcao pra configurar o que o express vai utilizar 
-
-
-
-
-app.listen(3000, ()=> { //inicializar o servidor. 1: numero da porta, 2: funcao pra ser executada quandop o servidor estiver rodando
-    console.log('server running')
-})
+app.listen(3000, () => {
+  //inicializar o servidor. 1: numero da porta, 2: funcao pra ser executada quandop o servidor estiver rodando
+  console.log("server running");
+});
